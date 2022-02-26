8a9ff8061af7859b16caccb7e207d1f1f4c6e0a5
jpod80 <jpodlesskih@gmail.com>
2022-02-27 00:47:35 +0300

    DZ_12_2

--- DZ_12_Podlesskih_gr112.py
+++ DZ_12_Podlesskih_gr112.py
@@ -10,5 +10,21 @@ print(d_dict)
 a = {i: i**3 for i in range(1, 11)}
 print(a)
 
-# Задание 2. Используя 2 списка создай новый словарь используя генератор.
+# Задание 3. Используя 2 списка создай новый словарь используя генератор.
+
+aaa_list = ['color', 'fruit', 'pet']
+bbb_list = ['blue', 'apple', 'dog']
+ccc_dict = {k: v for k, v in zip(aaa_list, bbb_list)}
+print(ccc_dict)
+
+# Задание 4. Функция нахождения минимального  значения произвольного количества аргументов
+
+def min_number(list_n):
+    min_n = min(list_n)
+    return min_n
+
+list_numbers = [10, 9]
+list_numbers1 = [2, 3, 4]
+list_numbers2 = [3, 5, 10, 6]
+print(min_number(list_numbers2))
 
