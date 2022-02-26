56d4cd2501216436ef5ac5f717113c02b395c1c0
jpod80 <jpodlesskih@gmail.com>
2022-02-26 22:59:46 +0300

    DZ_11

--- DZ_11_Podlesskih_gr112.py
+++ DZ_11_Podlesskih_gr112.py
@@ -1,8 +1,20 @@
 # # Задание 1.
 #
-# dic_a = {"name": "Kelly", "age": 25, "salary": 8000, "city": "New York"}
-#
-# print(dict.items(age, city))
+dic_a = {"name": "Kelly", "age": 25, "salary": 8000, "city": "New York"}
+dic_b = dict()
+dic_b["name"] = dic_a.pop("name")
+dic_b["salary"] = dic_a.pop("salary")
+print(dic_a)
+print(dic_b)
+print()
+
+# # Задание 2. Переименовать ключ.
+dic_c = {"name": "Kelly", "age": 25, "salary": 8000, "city": "New York"}
+# dic_c['location'] = dic_c.pop('city')  
+dic_ccc = {"location": "New York"}
+dic_c.pop("city")
+dic_c.update(dic_ccc)
+print(dic_c)
 
 
 
